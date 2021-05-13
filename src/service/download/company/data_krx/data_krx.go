cba15c3e6710842cc91e8b014e75d02d3034ac7c
cheolgyu <rb7480@knou.ac.kr>
2021-05-13 14:35:40 +0900

    포맷터 실행

--- src/service/download/company/data_krx/data_krx.go
+++ src/service/download/company/data_krx/data_krx.go
@@ -1,48 +1,48 @@
 package data_krx
 
 import (
-	"fmt"
 	"bytes"
-    "io/ioutil"
-    "net/http"
+	"fmt"
 	"io"
+	"io/ioutil"
+	"net/http"
 	"os"
 )
+
 // http://data.krx.co.kr/contents/MDC/MDI/mdiLoader/index.cmd?menuId=MDC0201020103
-	
+
 const get_code_url string = "http://data.krx.co.kr/comm/fileDn/GenerateOTP/generate.cmd"
 const get_file_url string = "http://data.krx.co.kr/comm/fileDn/download_excel/download.cmd"
 
 var (
-	fileName    string = "listed_company.xlsx"
+	fileName string = "listed_company.xlsx"
 )
 
 func Save() {
 	// Put content on file
-	down_file(down_code( ))
+	down_file(down_code())
 
 }
 
-func down_file(down_code string ){
+func down_file(down_code string) {
 	// 파일명
 	file := createFile()
 
-	
-	reqBody := bytes.NewBufferString("code="+down_code)
-    resp, err := http.Post(get_file_url, "application/x-www-form-urlencoded", reqBody)
-    if err != nil {
-        panic(err)
-    }
- 
-    defer resp.Body.Close()
- 
+	reqBody := bytes.NewBufferString("code=" + down_code)
+	resp, err := http.Post(get_file_url, "application/x-www-form-urlencoded", reqBody)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
 	size, err := io.Copy(file, resp.Body)
-	fmt.Println("counte=",size)
+	fmt.Println("counte=", size)
 	defer file.Close()
 
 	if err != nil {
-        panic(err)
-    }
+		panic(err)
+	}
 
 }
 
@@ -52,22 +52,22 @@ func createFile() *os.File {
 	return file
 }
 
-func down_code( ) string {
+func down_code() string {
 	reqBody := bytes.NewBufferString("mktId=ALL&share=1&csvxls_isNo=false&name=fileDown&url=dbms%2FMDC%2FSTAT%2Fstandard%2FMDCSTAT01901")
-    resp, err := http.Post(get_code_url, "application/x-www-form-urlencoded", reqBody)
-    if err != nil {
-        panic(err)
-    }
- 
-    defer resp.Body.Close()
- 
-    // Response 체크.
-    respBody, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        str := string(respBody)
-        println(str)
-    }
-	fmt.Println("down_code=",respBody)
+	resp, err := http.Post(get_code_url, "application/x-www-form-urlencoded", reqBody)
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	// Response 체크.
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		str := string(respBody)
+		println(str)
+	}
+	fmt.Println("down_code=", respBody)
 	return string(respBody)
 }
 
