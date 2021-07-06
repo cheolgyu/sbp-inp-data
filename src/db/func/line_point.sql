64823e69b7c22ee978bf2d35b85dc926298b552a
cheolgyu <rb7480@knou.ac.kr>
2021-07-06 19:18:40 +0900

    작업중

--- src/db/func/line_point.sql
+++ src/db/func/line_point.sql
@@ -14,9 +14,35 @@
 ----------------------------------------
 CREATE or replace FUNCTION public.update_lines()
  RETURNS void AS $$
+declare
+    row record;
+	x1 integer;
+	x2 integer;
+	x3 integer;
+	y1 integer;
+	y2 integer;
+	y3 integer;
+	arr_g_type  TEXT[]  :=  array['op','cp','lp','hp'];
 BEGIN
  -- code get
--- loop 
+	FOR row in 
+		select c.code ,c.name, d.market
+		from company.code c 
+			left join company.detail d on c.code= d.code 
+			left join company.state s on c.code=s.code 
+		where s.stop is false
+		order by code asc
+	loop
+		FOREACH g_type SLICE 1 IN ARRAY arr_g_type
+		LOOP
+			EXECUTE format(' SELECT * FROM  public.get_xy(%L ,%L) ', row.code, g_type ) INTO x1 ,y1 ,x2 ,y2;
+			EXECUTE format(' SELECT * FROM  public.get_x3_y3(%L ,%L ,%L ,%L ,%L ,%L ,%L) ', row.code ,row.market ,x1 ,y1 ,x2 ,y2 ) INTO x3 ,y3;
+			EXECUTE format(' SELECT * FROM  public.update_line_tb(%L ,%L ,%L ,%L ,%L ,%L ,%L ,%L) ', row.code ,row.market ,g_type ,x1 ,y1 ,x2 ,y2 ,x3 ,y3 ) ;
+		END LOOP;
+ 
+		
+		
+	end loop;	
 	--  public.get_xy(code text) into x1,y2,x2,y2 
 	--  public.get_x3_y3(code text) into x3 ,y3
 	--  public.insert_point(code text) into x3 ,y3
@@ -29,7 +55,8 @@ LANGUAGE plpgsql;
 ----------------------------------------
 ----------------------------------------
 ----------------------------------------
-
+-- 점 두개 찾기 g_type에 따라 
+-- 저가 면 아래에서 나머지는 위에서 
 --  select public.get_xy();
 
 ----------------------------------------
@@ -37,7 +64,7 @@ LANGUAGE plpgsql;
 ----------------------------------------
 ----------------------------------------
 ----------------------------------------
-CREATE OR REPLACE FUNCTION  public.get_xy(code text)
+CREATE OR REPLACE FUNCTION  public.get_xy(code text, g_type text )
  RETURNS TABLE (x1 integer  ,y1 integer ,x2 integer ,y2  integer ) AS
  $$
 BEGIN
@@ -45,7 +72,13 @@ BEGIN
 RETURN QUERY
  SELECT 1;
 
-
+--------------------
+select 
+g_type, x2,y2
+from hist.bound_stock
+where code = '000020' and g_type ='low'
+limit 90
+--------------------
 END;
 $$
 LANGUAGE plpgsql;
