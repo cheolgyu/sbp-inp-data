7bc6009a01431117a889978841883720ba1b2826
cheolgyu <rb7480@knou.ac.kr>
2021-07-19 17:46:10 +0900

    일단 company만

--- src/model/market.go
+++ src/model/market.go
@@ -18,18 +18,18 @@ type PriceMarket struct {
 	ForeignerBurnoutRate string
 }
 
-func (o *PriceMarket) ToPriceStock() PriceStock {
-	return PriceStock{
-		Date:                 o.Date,
-		OpenPrice:            int(o.OpenPrice),
-		HighPrice:            int(o.HighPrice),
-		LowPrice:             int(o.LowPrice),
-		ClosePrice:           int(o.ClosePrice),
-		Volume:               o.Volume,
-		ForeignerBurnoutRate: o.ForeignerBurnoutRate,
-	}
+// func (o *PriceMarket) ToPriceStock() PriceStock {
+// 	return PriceStock{
+// 		Date:                 o.Date,
+// 		OpenPrice:            int(o.OpenPrice),
+// 		HighPrice:            int(o.HighPrice),
+// 		LowPrice:             int(o.LowPrice),
+// 		ClosePrice:           int(o.ClosePrice),
+// 		Volume:               o.Volume,
+// 		ForeignerBurnoutRate: o.ForeignerBurnoutRate,
+// 	}
 
-}
+// }
 
 func (o *PriceMarket) StringToPrice(str string) {
 	arr := strings.Split(str, ",")
